9cbac4df360b8f7727365e920b705180ca7a9369
oio337a <oio337a@gmail.com>
2023-05-20 23:13:48 +0900

    Spring config with @Bean

---
+++ hello-spring/src/main/java/hi/hellospring/SpringConfig.java
@@ -0,0 +1,21 @@
+package hi.hellospring;
+
+import hi.hellospring.repository.MemberRepository;
+import hi.hellospring.repository.MemoryMemberRepository;
+import hi.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
